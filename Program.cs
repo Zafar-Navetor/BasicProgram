df0d2bfd10c917a589a30dddd2b668f8c57db8f8
zafar <zfrahmad@gmail.com>
2019-10-12 11:10:45 -0700

    My Dotnet Code

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+
+            int sum = a + b;
+            int Mul = a * b;
+            int sub = a - b;
+
+            Console.WriteLine($"sum = {sum} Mul = {Mul} sub = {sub}");
+            Console.ReadLine();
+
+            String FirstName = "Zafar";
+            String LastName = "Ahmad";
+            String FullName = FirstName + " " + LastName;
+
+            Console.WriteLine("My Name is:" + FullName);
+            Console.ReadLine();
+        }
+    }
+}
